ba57e68a1762fda1fcc4dbf8cbf5360bc1bb2d56
Magnus Jonsson <fot@fot.nu>
2018-10-23 22:52:43 +0200

    Todays playing...

--- app.py
+++ app.py
@@ -4,12 +4,39 @@ from models import *
 from playhouse.shortcuts import model_to_dict
 import json
 import uuid
+import jwt
+import datetime
 
 from falcon_auth import FalconAuthMiddleware, BasicAuthBackend
 
 user_loader = lambda username, password: { 'username': username }
 auth_backend = BasicAuthBackend(user_loader)
-auth_middleware = FalconAuthMiddleware(auth_backend, exempt_routes=['/exempt'], exempt_methods=['HEAD'])
+auth_middleware = FalconAuthMiddleware(auth_backend, exempt_routes=['/exempt','/jwt'], exempt_methods=['HEAD'])
+
+secret = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmb3QubnUiLCJyb2xlIjoiYWRtaW4iLCJzdWIiOiJrYWxsZSIsImV4cCI6MTU0MDMyNjEwMn0.PJBrMWt8VqUOZLBWSKDp-2ZFO-PLO-gT2Gmjkeu_-e0"
+alg = "HS256"
+
+class Jwt():
+    def on_post(self, req, resp):
+        if req.content_length:
+            doc = json.load(req.stream)
+
+        if 'user' in doc and 'password' in doc and doc['password'] == 'secret':
+            resp.body =  jwt.encode({
+                                    'sub': doc['user'], 
+                                    'iss': 'fot.nu', 
+                                    'role':'admin',
+                                    'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30)
+                                }, secret, algorithm=alg) + "\n"
+        else:
+            resp.body = json.dumps("BEeeep") + "\n"
+
+    def on_get(self, req, resp):
+        try:
+            resp.body = json.dumps(jwt.decode(req.params['jwt'], secret, algorithms=[alg]))
+        except Exception as e:
+            resp.body = json.dumps(str(e))
+
 
 class Auth():
     auth = {
@@ -47,12 +74,14 @@ class UserResource():
         resp.body = user.serialize()
         #json.dumps(model_to_dict(user))
 
-api = falcon.API(middleware=[auth_middleware,PeeweeConnectionMiddleware()])
+#api = falcon.API(middleware=[auth_middleware,PeeweeConnectionMiddleware()])
+api = falcon.API(middleware=[PeeweeConnectionMiddleware()])
 
 users = UserResource()
 users_id = UserIdResource()
 auth = Auth()
 
+api.add_route('/jwt', Jwt())
 api.add_route('/auth/', auth)
 api.add_route('/users/', users)
 api.add_route('/users/{user_id}', users_id)
